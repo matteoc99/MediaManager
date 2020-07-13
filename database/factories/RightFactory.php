fff0a43c0078fa7ab344aef8a6eecdc807096f25
matteo <matteo.cosi@live.it>
2020-07-13 17:36:37 +0200

    initial commit

---
+++ database/factories/RightFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Right;
+use Faker\Generator as Faker;
+
+$factory->define(Right::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
